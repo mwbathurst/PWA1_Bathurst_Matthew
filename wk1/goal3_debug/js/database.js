e74772b25dd0e9b625526c6466e44d6f961bb1c1
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-08 12:24:49 -0700

    Matthew Bathurst Programming for Web Applications I - Online Instructor: Fialishia OLoughlin Assignment: Week #1 :: { Homework } - DEBUG IT! v2 (4h) Assignment Date:  May 5 - May 11, 2014 (Week 1)
    Setting Files Up.

---
+++ wk1/goal3_debug/js/database.js
@@ -0,0 +1,32 @@
+
+/*
+Matthew Bathurst
+Programming for Web Applications I - Online
+Instructor: Fialishia OLoughlin
+Assignment: Week #1 :: { Homework } - DEBUG IT! v2 (4h)
+Assignment Date:  May 5 - May 11, 2014 (Week 1)
+File: Database.js
+*/
+var db = [
+	"JavaScript Version History|http://http://wddbs.com/javascript/sfw/online/#p=videos&s=3&file=01.html",
+	"JavaScript in the Browser|http://wddbs.com/javascript/sfw/online/#p=videos&s=3&file=01.html",
+	"Script Tag|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"JavaScript Syntax|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Data Type Literals|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Strings & HTML Strings|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Numbers|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Arrays|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Objects|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Functions|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Scope|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Loops|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Conditionals|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Typeof|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Timers|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Native Objects|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
+	"Debugging Tools|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",
+	"Firebug|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",
+	"Error Types|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",
+	"Closures|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=04.html",
+	"Context|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=04.html"
+];
