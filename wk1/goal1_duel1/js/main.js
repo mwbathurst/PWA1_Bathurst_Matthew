fefb6653520bcef110a699b7944d17c730a7676d
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-08 16:31:05 -0700

    Setting up Week #1 :: { Homework } - THE DUEL #1 (4h)

---
+++ wk1/goal1_duel1/js/main.js
@@ -0,0 +1,11 @@
+/*
+ Matthew Bathurst
+ Programming for Web Applications I - Online
+ Instructor: Fialishia OLoughlin
+ Assignment: Week Week #1 :: { Homework } - THE DUEL #1 (4h)
+ Assignment Date:  May 5 - May 11, 2014 (Week 1)
+ Date Finished: Thursday May 8, 2014
+ file: main.js
+
+
+ */
