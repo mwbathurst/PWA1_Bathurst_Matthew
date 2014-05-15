14d1b189391810a0558b3effadd2be5585e4c540
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-14 22:39:03 -0700

    First Set of Variables defined

--- wk2/goal4_assign_guess/js/main.js
+++ wk2/goal4_assign_guess/js/main.js
@@ -1,14 +1,43 @@
-/**
- * GUESSING GAME:
- *
- * Created By: 
- * Date: 
- * 
- * GUESSING GAME
+/*
+ Matthew Bathurst
+ Programming for Web Applications I - Online
+ Instructor: Fialishia OLoughlin
+ Assignment: Week #2 :: { Homework } - GUESSING GAME (2h)
  */
 
 //Game variables
-(function ()
+(function (){
+
+	//Defines Limits of Random Number Calculation for future modifcations
+	var magicLimits = {
+			max : 10,
+			min : 1
+		}
+
+		
+	// Calculation of the Magic Number using the Max and Min ceiling/floor defined in magicLimits
+	var magicNum = Math.floor((Math.random() * magicLimits.max) +magicLimits.min);
+
+	// Console.log for testing code
+
+	console.log(magicNum);
+	// Variable setup for the GAME
+	var dom ={
+		input : document.querySelector('#input'),
+		output : document.querySelector('#output'),
+		button : document.querySelector('button'),
+	};
+
+	
+
+
+
+
+
+
+
 
 
 })();
+
+
