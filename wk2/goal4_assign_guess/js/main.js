1d2f8ce74f7199eb082fc9d6a30499d43a534b9e
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-15 00:03:41 -0700

    Input Validation Test with doc.output.innerHTML conditional statement

--- wk2/goal4_assign_guess/js/main.js
+++ wk2/goal4_assign_guess/js/main.js
@@ -36,18 +36,48 @@
 		made:0
 	};
 	//Function that runs the game
-	var click = function(){
+	/*var click = function(){
 		console.log(guess.limit);
 
 	};
+	*/
 
-	var myfn = function(){
+
+	var numberVal = function(){
 		guess.player = parseInt(dom.input.value);
-		console.log(guess.limit, guess.player);
-	}
-	dom.button.onclick = myfn;
 
+		if(isNaN(guess.player)){
+			dom.output.innerHTML = "Please Enter a Number between " + magicLimits.min + " and " + magicLimits.max;
+		}else if(guess.player < magicLimits.min){
+			dom.output.innerHTML = "You have entered a number less than " + magicLimits.min + ". Please Enter a Number between " + magicLimits.min + " and " + magicLimits.max;
+		}else if(guess.player > magicLimits.max){
+			dom.output.innerHTML = "You have entered a number greater than " + magicLimits.max + ". Please Enter a Number between " + magicLimits.min + " and " + magicLimits.max;
+		}else{
+			game();
+		}
+	}
+
+
+
+
+	var game = function(){
+		guess.player = parseInt(dom.input.value);
 	
+		guess.limit--;
+		if(guess.limit < 0){
+			console.log("You've Reached your limit");
+			dom.button.removeEventListener('click', myfn, false);
+		}else{	
+			console.log(guess.limit, guess.player);
+
+		}
+	}
+	
+
+	//clicking test
+	//dom.button.onclick = myfn;
+
+	dom.button.addEventListener('click', numberVal, false);
 })();
 
 
