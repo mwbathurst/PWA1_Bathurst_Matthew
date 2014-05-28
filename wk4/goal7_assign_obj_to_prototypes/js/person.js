526a4c449b0769eb02a5a9676498c0881d056b87
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-28 11:20:29 -0500

    final push

--- wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -7,24 +7,33 @@ file: person.js
  */
 
 (function(){
-
+//global calling
 window.Person = Person;
-Person.jobs = ["he Don", "Fixer", "Soldier"];
+//defines jobs & actions that can be called for the site
+Person.jobs = ["The Don", "Fixer", "Soldier", "Wheel Man", "Snitch"];
 Person.actions = ["Wacking", "Smacking", "Jacking", "Hitting", "Sleeping", "Doing Time"];
 
 
 function Person(name, row){
 	console.log("Person Created: ", name);
 	this.name = name;
-	this.action = Person.action[Math.floor(Math.random()*Person.jobs.length)];
+	this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
+	this.action = Person.actions[Math.floor(Math.random()*Person.jobs.length)];
 	this.row = row;
 
 	var id = document.getElementById("r" +this.row+"c3");
 	id.innerHTML = this.action;
 
 }
-
-
+//random updated based on time
+Person.prototype.update = function(){
+	if(Math.floor(Math.random() < .01)){
+		var i = Math.floor(Math.random() * Person.actions.length);
+		this.action = Person.actions[i];
+		var id = document.getElementById("r" +this.row+"c3");
+		id.innerHTML = this.action;
+	}
+}
 
 
 
