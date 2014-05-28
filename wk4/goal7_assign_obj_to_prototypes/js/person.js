75cee46fe2ab570f193ad9d392a52a7148abf99c
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-28 10:51:17 -0500

    random name outputted to console.log

--- wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -14,8 +14,17 @@ Person.actions = ["Wacking", "Smacking", "Jacking", "Hitting", "Sleeping", "Doin
 
 
 function Person(name, row){
+	console.log("Person Created: ", name);
+	this.name = name;
+	this.action = Person.action[Math.floor(Math.random()*Person.jobs.length)];
+	this.row = row;
+
+	var id = document.getElementById("r" +this.row+"c3");
+	id.innerHTML = this.action;
 
 }
+
+
 
 
 
