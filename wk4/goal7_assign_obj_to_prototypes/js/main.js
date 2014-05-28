89b4aad790da58366222d64ffe77eb19a19554ff
Matthew Bathurst <mwbathurst@fullsail.edu>
2014-05-28 11:18:38 -0500

    working

--- wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -8,12 +8,39 @@ file: main.js
 
 (function(){
 
-var capPeople = 5;
+// Global Variables defined
+var capPeople = 3;
 var people = [];
 var names = ["Vito Corleone", "Michael Corleone", "Sonny Corleone", "Fredo Corleone", "Tom Hagen"];
 var interval;
 
+// Main output for site in HTML
+for (var i = 0; i < capPeople ; i++){
+	var index = Math.floor(Math.random() * names.length);
+	var person = new Person(names[index], i+1);
+	//populates the HTML with the info
+	populateHTML(person.name,"r"+(i+1)+"c1");
+	populateHTML(person.job,"r"+(i+1)+"c2");
 
+	people.push(person);
+	//Code that allows for no repeat in names
+	names.splice(index,1);
+}
+
+//set the interval as the assignment calls for 
+interval = setInterval(runUpdate, 1000 / 30);
+
+
+function populateHTML(data, field){
+	var id = document.getElementById(field);
+	id.innerHTML = data;
+}
+
+function runUpdate(){
+	people.forEach(function(element){
+		element.update();
+	});
+}
 
 
 
